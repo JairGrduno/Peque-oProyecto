c73d08f721c08bf107bc6d0de29cc02590a4c17f
Jair Garduño <89103304+JairGrduno@users.noreply.github.com>
2022-10-09 21:04:09 -0500

    Actualizacion busqueda en amplitud

--- IA/arbol.py
+++ IA/arbol.py
@@ -6,45 +6,45 @@ class Nodo:
         self.coste = None
         self.setHijos = (hijos)
 
-    def setHijos(self, hijos): #Asigna al nodo la lista de hijos que osn pasados con parametro
+    def set_Hijos(self, hijos): #Asigna al nodo la lista de hijos que osn pasados con parametro
         self.hijos = hijos
         if self.hijos != None:
-            for a in self.hijos:
-                a.padre = self
+            for h in self.hijos:
+                h.padre = self
 
-    def getHijos(self): #Retorna una lista con los hijos del nodo
+    def get_Hijos(self): #Retorna una lista con los hijos del nodo
         return self.hijos
 
-    def getPadre(self): #Retorna el nodo padre
+    def get_Padre(self): #Retorna el nodo padre
         return self.padre
 
-    def setPadre(self, padre): #Asigna el nodo padre de este nodo
+    def set_Padre(self, padre): #Asigna el nodo padre de este nodo
         self.padre = padre
 
-    def setDatos(self, datos): #Asigna un dato a un nodo
+    def set_Datos(self, datos): #Asigna un dato a un nodo
         self.datos = datos
 
-    def getDatos(self): #Devuelve el dato almacenado de un nodo
+    def get_Datos(self): #Devuelve el dato almacenado de un nodo
         return self.datos
 
-    def setCoste(self, coste): #Asigna un peso al nodo dentro del arbol
+    def set_Coste(self, coste): #Asigna un peso al nodo dentro del arbol
         self.coste = coste
 
-    def getCoste(self): #Devuelve el peso del nodo dentro del arbol
+    def get_Coste(self): #Devuelve el peso del nodo dentro del arbol
         return self.coste
 
     def igual(self, nodo): #Devuelve True si el dato contenido en el nodo es igual al nodo pasado por parametro
-        if self.getDatos() == nodo.getDatos():
+        if self.get_Datos() == nodo.get_Datos():
             return True
         else:
             return False
 
-    def enlista(self, lista_nodos): #Devuelve True si el dato contenido en el nodo es igual a algunoi de los nodos contenidos en la lista de nodos pasada por parametros 
+    def en_lista(self, lista_nodos): #Devuelve True si el dato contenido en el nodo es igual a algunoi de los nodos contenidos en la lista de nodos pasada por parametros 
         en_la_lista = False
         for n in lista_nodos:
             if self.igual(n):
                 en_la_lista = True
         return en_la_lista
 
-    def __str__(self) -> str:
-        return str(self.getDatos())
+    def __str__(self):
+        return str(self.get_Datos())
--- IA/puzzlelineal.py
+++ IA/puzzlelineal.py
@@ -10,30 +10,30 @@ def buscar_solucion_BFS(estado_inicial,solucion):
         nodo=nodos_frontera.pop(0)
         #Extraer nodo y añadirlo a visitados
         nodos_visitados.append(nodo)
-        if nodo.getDatos()==solucion:
+        if nodo.get_Datos()==solucion:
             #Solucion encontrada
             solucionado=True
             return nodo
         else:
             #Expandir nodos hijos
-            dato_nodo=nodo.getDatos()
-
+            dato_nodo=nodo.get_Datos()
+            
             #Operados izquierda
             hijo=[dato_nodo[1],dato_nodo[0],dato_nodo[2],dato_nodo[3]]
             hijo_izquierdo=Nodo(hijo)
-            if not hijo_izquierdo.enlista(nodos_visitados) and not hijo_izquierdo.enlista(nodos_frontera):
+            if not hijo_izquierdo.en_lista(nodos_visitados) and not hijo_izquierdo.en_lista(nodos_frontera):
                 nodos_frontera.append(hijo_izquierdo)
             #Operador central
             hijo=[dato_nodo[0],dato_nodo[2],dato_nodo[1],dato_nodo[3]]
             hijo_central=Nodo(hijo)
-            if not hijo_central.enlista(nodos_visitados) and not hijo_central.enlista(nodos_frontera):
+            if not hijo_central.en_lista(nodos_visitados) and not hijo_central.en_lista(nodos_frontera):
                 nodos_frontera.append(hijo_central)
             #Operador derecho
             hijo=[dato_nodo[0],dato_nodo[1],dato_nodo[3],dato_nodo[2]]
             hijo_derecho=Nodo(hijo)
-            if not hijo_derecho.enlista(nodos_visitados) and not hijo_central.enlista(nodos_frontera):
+            if not hijo_derecho.en_lista(nodos_visitados) and not hijo_central.en_lista(nodos_frontera):
                 nodos_frontera.append(hijo_derecho)
-            nodo.setHijos([hijo_izquierdo,hijo_central,hijo_derecho])
+            nodo.set_Hijos([hijo_izquierdo,hijo_central,hijo_derecho])
 if __name__=='__main__':
     estado_inicial=[4,2,3,1]
     solucion=[1,2,3,4]
@@ -41,10 +41,9 @@ if __name__=='__main__':
     #Mostrar_resultado
     resultado=[]
     nodo=nodo_solucion
-    print()
-    while nodo.getPadre() != None:
-        resultado.append(Nodo.getDatos)
-        nodo=nodo.getPadre()
+    while nodo.get_Padre() != None:
+        resultado.append(nodo.get_Datos)
+        nodo=nodo.get_Padre()
     resultado.append(estado_inicial)
     resultado.reverse()
     print(resultado)
